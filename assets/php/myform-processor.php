0861d7cc2ee2e920f4b7da4f6462b7feb3e2fcf5
Ana Laura Moreno <almoreno@Anas-MacBook-Pro.local>
2021-06-29 10:46:03 -0500

    testing out email form with php

---
+++ assets/php/myform-processor.php
@@ -0,0 +1,17 @@
+<?php
+
+
+if($_POST["message"]) {
+
+
+mail("almoreno@themorenofamily.com", "Here is the subject line",
+
+
+$_POST["insert your message here"]. "From: an@email.address");
+
+
+}
+
+
+?>
+
